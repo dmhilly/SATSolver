8444bbd51d38976a4ffff254e2fa5491916bb56a
Devin Hilly <devinhilly@gmail.com>
2017-11-30 13:52:50 -0500

    Cleaned up backtracking

--- src/dpll.scala
+++ src/dpll.scala
@@ -1,8 +1,8 @@
 // TODO: 
-// understand 2-literal watching
-// should be checking for whether both vars have been tried, not just if has been set
-// figure out the whole backtracking scheme
-// how and when to check whether or not program is satisfiable
+// write propogateAssignment
+// look into scala pointers so you don't have to return program, etc, every time
+// make sure you're checking for satisfiability and unit clauses in the right places and in right way
+// do thorough walk-through of code and test
 
 import java.io._
 import scala.io.BufferedSource
@@ -40,15 +40,6 @@ object DPLL {
 		return (true, unitLit)
 	}
 
-	/* Return variable value that makes literal true. */
-	def setToTrue(literal: Int): Int = {
-		if (literal < 0){
-			return 0
-		} else{
-			return 1
-		}
-	}
-
 	/* Indicates whether the current assignment of variables is satisfiable. */
 	def isSatisfiable(program: Program): ProgramStatus = {
 		for (assignment <- program.varVals){
@@ -62,7 +53,7 @@ object DPLL {
 				// if literal evaluates to 1, isSat = true, break
 				if (literal.negated == true && varVals(literal.varNum) == 0 ||
 					literal.negated == false && varVals(literal.varNum) == 1){
-					isSat = true // TODO: break out early from this
+					isSat = true
 				}
 			}
 			if (!isSat){
@@ -72,93 +63,61 @@ object DPLL {
 		return Satisfiable
 	}
 
-	def setVariable(program: Program){
-
+	def propogateAssignment(): ProgramStatus = {
+		// propogate assignment
+		// if conflict, return Conflict
+		// return isSatisfiable
 	}
 
-	// deduce:
-	// set next var (for now this is next in order)
-	// apply unit propogation
-	// if status == conflict,
-		// add current assignment to hash table 
-		// unset var and any other vars which were set during propogation
-	// repeat process above setting to other value
-	// if no luck, backtrack to highest level where both values of that var have not been tried
-
-
-	// backtrack:
-	// if var = not(assignment in stack) + assignments below it in stack has been tried,
-	// pop it off and backtrack
-	
-
-	/* Apply unit propogation (set a literal and propogate its implications) */
-	def deduce(program: Program): ProgramStatus = {
-		program.varVals = setVariable(program)
-		var setVariables = program.blevels(program.blevels.size)
-		setVariables(highLiteral) = 1
-		// for clause in program, if is unit clause, apply unit clause rule
-		for (clause <- program.clauses){
-			var unitClause = isUnit(clause, program.varVals)
-			if (isUnit(clause)._1){
-				var literal = unitClause._2
-				var newLitValue = setToTrue(literal)
-				var oldLitValue = program.varVals(Math.abs(literal))
-				if (oldLitValue != -1 && oldLitValue != newLitValue){
-					return (Conflict, program)
+	/* Set a variable, propogate its implications, detect conflicts, and backtrack. */
+	def deduce(program: Program, configStack: scala.collection.mutable.Stack[(Int, Boolean)],
+		variable: Int, triedConfigs: scala.collection.mutable.set[Set((Int, Boolean))]): ProgramStatus = {
+		for (value <- (1, 0)) {
+			var newConfig = configStack.toSet() :+ (variable, value)
+			if (!triedConfigs.contains(newConfig)) {
+				program.varVals(variable) = value
+				var (status, variablesSet) = propogateAssignment(program)
+				if (status == Conflict){
+					triedConfigs.add(newConfig) // add to hashtable
+					program.varVals = unsetVars(variablesSet, program)
+				} else{
+					configStack.push((variable, value)) // add assignment to config stack
+					return status
 				}
-				program.varVals(Math.abs(literal)) = newLitValue
-				setVariables(Math.abs(literal)) = 1
+			}
+			if (val == 0){ // tried both
+				return backtrack(configStack)
 			}
 		}
-		// update level
-		program.blevels += (program.blevels.size + 1 -> setVariables)
-		// check satisfiability
-		if (isSatisfiable(program)){
-			return (Satisfiable, program)
-		} else {
-			return (Unknown, program)
+	}
+
+	/* Backtrack to the highest level such that both values of the variable have not been tried. */
+	def backtrack(program: Program, configStack: scala.collection.mutable.Stack[(Int, Boolean)],
+		triedConfigs: scala.collection.mutable.set[Set((Int, Boolean))]): ProgramStatus = {
+		if (configStack.isEmpty){
+			return Unsatisfiable
 		}
-	}
-
-	/* ? */
-	def analyzeConflict(): Int = {
-		return 1;
-	}
-
-	/* Backtrack to the highest level which has not tried both values. */
-	def backtrack(blevel: Int): Unit = {
-
+		var highestAssignment = configStack.top()
+		if (triedConfigs.contains((highestAssignment._1, !highestAssignment._2))){
+			configStack.pop()
+			return backtrack(configStack)
+		} else {
+			return deduce(program, configStack, highestAssignment._1, triedConfigs)
+		}
 	}
 
 	/* Implementation of the DPLL algorithm. */
 	def DPLL(program: Program): ProgramStatus = {
 		program = setUniqueVars(program);
 		var status = isSatisfiable(program);
-		if (status != Unknown){
-			return status
+		var configStack = scala.collection.mutable.Stack[(Int, Boolean)]
+		var triedConfigs = scala.collection.mutable.Set[Set(Int, Boolean)]
+		while (status == Unknown) {
+			var results = deduce(program, configStack, configStack.size, triedConfigs)
+			status = results._1
+			program = results._2
 		}
-		var loop = true;
-		var setVariables = scala.collection.mutable.Stack[(Int, Boolean)]
-		var setNext = 0;
-		while (loop) {
-			while (true) {
-				var results = deduce(program)
-				status = results._1
-				program = results._2
-				if (status == Conflict) {
-					var blevel = analyzeConflict(program, highLiteral)
-					if (blevel < 0){
-						return Unsatisfiable
-					} else {
-						backtrack(blevel)
-					}
-				} else if (status == Satisfiable) {
-					return Satisfiable
-				} else {
-					loop = false // ?? could be wrong
-				}
-			}
-		}
+		return status
 	}
 
 	/* Returns a map which contains each literal in the program and the number of times it appears. */
